fbeace4351fa6837be0998ce3ce18ad6f343cbb8
shellrow <81893184+shellrow@users.noreply.github.com>
2022-04-24 23:39:54 +0900

    Add dns-lookup and RTT measurement

--- src/trace.rs
+++ src/trace.rs
@@ -1,5 +1,5 @@
 use std::net::IpAddr;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use socket2::{Domain, Protocol, Socket, Type};
 use std::mem::MaybeUninit;
 use std::net::{SocketAddr, UdpSocket};
@@ -72,6 +72,7 @@ impl Tracer {
             let mut buf: Vec<u8> = vec![0; 512];
             let mut recv_buf = unsafe { &mut *(buf.as_mut_slice() as *mut [u8] as *mut [MaybeUninit<u8>]) };
             let dst: SocketAddr = SocketAddr::new(self.dst_ip, BASE_DST_PORT + ttl as u16);
+            let send_time = Instant::now();
             match udp_socket.send_to(&udp_buf, dst) {
                 Ok(_) => (),
                 Err(e) => {
@@ -80,29 +81,32 @@ impl Tracer {
             }
             match icmp_socket.recv_from(&mut recv_buf) {
                 Ok((bytes_len, _addr)) => {
+                    let recv_time = Instant::now().duration_since(send_time);
                     //println!("{} {} {}", ttl, bytes_len, addr.as_socket_ipv4().unwrap());
                     let recv_buf = unsafe { *(recv_buf as *mut [MaybeUninit<u8>] as *mut [u8; 512]) };
                     if let Some(packet) = pnet_packet::ipv4::Ipv4Packet::new(&recv_buf[0..bytes_len]){
                         let icmp_packet = pnet_packet::icmp::IcmpPacket::new(packet.payload());
                         if let Some(icmp) = icmp_packet {
+                            let ip_addr: IpAddr = IpAddr::V4(packet.get_source());
+                            let host_name: String = dns_lookup::lookup_addr(&ip_addr).unwrap_or(ip_addr.to_string());
                             match icmp.get_icmp_type() {
                                 IcmpTypes::TimeExceeded => {
                                     result.push(Node {
-                                        ip_addr: IpAddr::V4(packet.get_source()),
-                                        host_name: packet.get_source().to_string(),
+                                        ip_addr: ip_addr,
+                                        host_name: host_name,
                                         hop: ttl,
                                         node_type: if ttl == 1 {NodeType::DefaultGateway}else{NodeType::Relay},
-                                        rtt: Duration::from_millis(0),
+                                        rtt: recv_time,
                                     });
                                     //println!("{} TimeExceeded {:?}", ttl, packet.get_source());
                                 },
                                 IcmpTypes::DestinationUnreachable => {
                                     result.push(Node {
-                                        ip_addr: IpAddr::V4(packet.get_source()),
-                                        host_name: packet.get_source().to_string(),
+                                        ip_addr: ip_addr,
+                                        host_name: host_name,
                                         hop: ttl,
                                         node_type: NodeType::Destination,
-                                        rtt: Duration::from_millis(0),
+                                        rtt: recv_time,
                                     });
                                     //println!("{} DestinationUnreachable {:?}", ttl, packet.get_source());
                                     break;
