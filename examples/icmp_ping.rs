a989376ce3eb291e57eab4d419fcdd7dc73f8c8e
shellrow <shellrow@foctal.com>
2026-02-14 18:12:23 +0900

    refactor: auto-select IPv4/IPv6 in examples

--- examples/icmp_ping.rs
+++ examples/icmp_ping.rs
@@ -2,12 +2,21 @@ use std::net::{IpAddr, Ipv4Addr};
 use std::time::Duration;
 use tokio::time::sleep;
 use tracert::ping::Pinger;
+use tracert::protocol::Protocol;
 
 #[tokio::main]
 async fn main() {
     // ICMPv4 ping to cloudflare's one.one.one.one (1.1.1.1)
     let dst_ip: IpAddr = IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1));
-    let pinger: Pinger = Pinger::new(dst_ip).unwrap();
+    // ICMPv6 ping to cloudflare's one.one.one.one (2606:4700:4700::1111)
+    //let dst_ip: IpAddr = IpAddr::V6(Ipv6Addr::new(0x2606, 0x4700, 0x4700, 0, 0, 0, 0, 0x1111));
+
+    let mut pinger: Pinger = Pinger::new(dst_ip).unwrap();
+    pinger.set_protocol(if dst_ip.is_ipv4() {
+        Protocol::Icmpv4
+    } else {
+        Protocol::Icmpv6
+    });
     let rx = pinger.get_progress_receiver();
 
     let handle = tokio::spawn(async move { pinger.ping_async().await });
